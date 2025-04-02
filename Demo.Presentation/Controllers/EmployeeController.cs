68e35263ec9b9a60b7ec13d2dd4270d5b58c4c43
Abdallah Bahaa <abdallahbahaa.200465@gmail.com>
2025-04-03 00:27:16 +0200

    Part 05 Employee Module - Create Action

--- Demo.Presentation/Controllers/EmployeeController.cs
+++ Demo.Presentation/Controllers/EmployeeController.cs
@@ -1,14 +1,54 @@
-﻿using Demo.BusinessLogic.Services.Interfaces;
+﻿using Demo.BusinessLogic.DataTransferObjects.DepartmentDTOS;
+using Demo.BusinessLogic.DataTransferObjects.EmployeeDTO;
+using Demo.BusinessLogic.Services.Classes;
+using Demo.BusinessLogic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Presentation.Controllers
 {
-    public class EmployeeController(IEmployeeSerivces employeeSerivces) : Controller
+    public class EmployeeController(IEmployeeSerivces employeeSerivces,ILogger<EmployeeController> _logger, IWebHostEnvironment _environment) : Controller
     {
         public IActionResult Index()
         {
             var employees = employeeSerivces.GetAllEmployees(); 
             return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+             
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateEmploeeDto emploeeDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = employeeSerivces.CreateEmoplyee(emploeeDto);
+
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    if (_environment.IsDevelopment())
+                    {
+                        ModelState.AddModelError(string.Empty, ex.Message);
+
+                    }
+                    else
+                    {
+                        _logger.LogError(ex.Message);
+                    }
+                }
+            }
+
+            return View(emploeeDto);
+
         }
     }
 }
