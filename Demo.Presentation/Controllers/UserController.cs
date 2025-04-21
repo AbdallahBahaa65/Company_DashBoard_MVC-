ac84e5710c030694908a6827812eb91f2c81ca91
Abdallah Bahaa <abdallahbahaa.200465@gmail.com>
2025-04-21 03:56:21 +0200

    User And Role Controllers With Actions

---
+++ Demo.Presentation/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Demo.DataAccess.Models.IdentiyModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class UserController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager) : Controller
+    {
+        public IActionResult Index(string? UserSearchName)
+        {
+
+            var users = _userManager.Users.AsQueryable(); 
+
+            if (!string.IsNullOrEmpty(UserSearchName))
+            {
+                users = users.Where(u => u.UserName.Contains(UserSearchName));
+            }
+            return View(users.ToList());
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Details(string Id)
+        {
+            var UserDetails = _userManager.FindByIdAsync(Id).Result;
+            return View(UserDetails);
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult Update(string Id)
+        {
+            
+            var UserUpadte = _userManager.FindByIdAsync(Id).Result;
+            if (UserUpadte is not null)
+                return View(UserUpadte);
+            else
+                return NotFound();
+        }
+
+   
+        [HttpPost]
+        public IActionResult Update(string Id,ApplicationUser applicationUser)
+        {
+                
+            if (!ModelState.IsValid) return View(applicationUser);
+            
+            var user =  _userManager.FindByIdAsync(Id).Result;
+            if (user == null)
+                return NotFound();
+
+
+            user.FirstName = applicationUser.FirstName;
+            user.LastName = applicationUser.LastName;
+            user.PhoneNumber = applicationUser.PhoneNumber;
+
+            var Result = _userManager.UpdateAsync(user).Result;
+
+            if (Result.Succeeded)
+                return RedirectToAction("Index"); 
+
+            foreach (var error in Result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(applicationUser);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string Id)
+        {
+            var UserDelete = _userManager.FindByIdAsync(Id).Result;
+            return View(UserDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string Id , ApplicationUser applicationUser)
+        {
+
+            if (!ModelState.IsValid) return View(applicationUser);
+
+            var user = _userManager.FindByIdAsync(Id).Result;
+            if (user == null)
+                return NotFound();
+
+
+            var Result = _userManager.DeleteAsync(user).Result;
+
+            if (Result.Succeeded)
+                return RedirectToAction("Index"); 
+
+            foreach (var error in Result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(applicationUser);
+        }
+    }
+}
